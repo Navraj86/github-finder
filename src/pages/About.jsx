8089fd8eba59a7d03ed5646c93f2d895a9777b88
Navraj Singh <navrajbanga86@gmail.com>
2024-12-12 15:14:23 +0530

    About Section Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ function About() {
         <h1 className='text-6xl mb-4'>Github Finder</h1>
         <p className='mb-4 text-2xl font-light'>
           A React app to search GitHub profiles and see profile details. This
-          project is made by <strong>Navraj Singh</strong>.
+          project is made by <strong>Navraj Singh and Team</strong>.
         </p>
         <p className='text-lg text-gray-400'>
           Version <span className='text-white'>1.0.0</span>
